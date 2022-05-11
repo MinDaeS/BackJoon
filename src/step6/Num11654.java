82c6b3247833f5dcd7b25cfebd0bb6c04fd57a0b
MinDaes <made0676@gmail.com>
2022-05-11 10:50:01 +0900

    아스키 코드 변환  - 숏코드 수정

--- src/step6/Num11654.java
+++ src/step6/Num11654.java
@@ -1,5 +1,6 @@
 package step6;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
@@ -8,8 +9,12 @@ import java.util.Scanner;
  */
 public class Num11654 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        System.out.println(System.in.read());
+    }
+
+    public void try1(){
         Scanner scanner = new Scanner(System.in);
         String text = scanner.next();
         System.out.println(text.getBytes(StandardCharsets.US_ASCII)[0]);
