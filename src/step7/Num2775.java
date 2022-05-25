15a4722426b367776363ba024f4f6b17ad124b58
MinDaes <made0676@gmail.com>
2022-05-25 14:36:20 +0900

    부녀회장이 될테야 - 사칙연산, 구현

---
+++ src/step7/Num2775.java
@@ -0,0 +1,17 @@
+package step7;
+
+import java.io.*;
+
+public class Num2775 {
+
+    /**
+     * 부녀회장이 될테야 - 사칙연산, 구현
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
