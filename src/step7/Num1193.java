ada858e12bcced366ed78f8b039d3f75df57311b
MinDaes <made0676@gmail.com>
2022-05-25 14:33:59 +0900

    주석 추가

--- src/step7/Num1193.java
+++ src/step7/Num1193.java
@@ -4,6 +4,11 @@ import java.io.*;
 
 public class Num1193 {
 
+    /**
+     * 달팽이는 올라가고 싶다
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
